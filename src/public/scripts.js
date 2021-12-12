c3e3bf94e6dd70ef49ab77ac63c611b352d79ebb
yael1982 <yael.courty@yahoo.com.ar>
2021-12-12 18:41:11 -0300

    responsive

---
+++ src/public/scripts.js
@@ -0,0 +1,12 @@
+let hamburguesa=document.querySelector(".hamburguesa")
+let menu=document.querySelector(".barra-nav");
+
+
+hamburguesa.addEventListener("click",()=>{
+ 
+    menu.classList.toggle("showMenu");
+    
+
+});
+
+console.log("Hola");
